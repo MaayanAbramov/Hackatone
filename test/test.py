0d48867baaf591de89293aa4d85e731982afcb2b
KareemAraide <KareemAraide@gmail.com>
2022-05-19 14:59:47 +0300

    update

--- test/test.py
+++ test/test.py
@@ -15,7 +15,7 @@ play_audio_last = 0.0
 epsilon = 1.0
 
 cap = cv2.VideoCapture(0)
-left_profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
@@ -41,26 +41,36 @@ def play_audio(audio_file):
 def main():
     while True:
         ret, frame = cap.read()
+        frame_fliped = frame
         frame = cv2.flip(frame, 1)
         width = int(cap.get(3))
         height = int(cap.get(4))
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray_fliped = cv2.cvtColor(frame_fliped, cv2.COLOR_BGR2GRAY)
         scaleFactor=1.2
         minNeighbors=5
         lineThickness=3
         green=(0, 255, 0)
         blue=(255, 0, 0)
+        blue_green=(255,255,0)
+        blue_red=(255,0,255)
         red=(0, 0, 255)
         faces = face_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
-        left_profile_faces = left_profile_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
-        #t0 = time.clock()
-        if len(faces) == 0:# and not p.is_alive():
-            #p = multiprocessing.Process(target=playsound, args='lostFace.mp3').start()
-            play_audio('lostFace.mp3')
-            #time.sleep(0.2)
-        """
+        right_profile_cascade = profile_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
+        left_profile_cascade = profile_cascade.detectMultiScale(gray_fliped, scaleFactor, minNeighbors)
+        for(x, y, w, h) in right_profile_cascade:
+            play_audio('adjustEyesRight.mp3')
+            cv2.rectangle(frame, (x, y), (x + w, y + h), blue_green, lineThickness)
+        for(x, y, w, h) in left_profile_cascade:
+            play_audio('adjustEyesLeft.mp3')
+            cv2.rectangle(frame, (x, y), (x + w, y + h), blue_red, lineThickness)
         for (x, y, w, h) in faces:
+            #t0 = time.clock()
+            if len(faces) == 0:# and not p.is_alive():
+                #p = multiprocessing.Process(target=playsound, args='lostFace.mp3').start()
+                play_audio('lostFace.mp3')
+                #time.sleep(0.2)
             cv2.rectangle(frame, (x, y), (x + w, y + h), blue, lineThickness)
             faceCenterXAxis=x+w//2
             roi_gray = gray[y:y+w, x:x+w]
@@ -71,13 +81,14 @@ def main():
                 eyeCenterXAxis=x+ex+ew//2
                 eyeCenterYAxis=y+ey+eh//2
                 cv2.line(frame, (eyeCenterXAxis, 0), (eyeCenterXAxis, height), red, lineThickness)
-                cv2.line(frame, (0, eyeCenterYAxis), (width, eyeCenterYAxis), red, lineThickness)       
+                cv2.line(frame, (0, eyeCenterYAxis), (width, eyeCenterYAxis), red, lineThickness)
+                """
                 if len(eyes)==1:
                     if eyeCenterXAxis > faceCenterXAxis:
                         play_audio('adjustEyesRight.mp3')
                     elif eyeCenterXAxis < faceCenterXAxis:
                         play_audio('adjustEyesLeft.mp3')
-        """
+                """
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) == ord('q'):
             break
